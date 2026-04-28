89f4b1925f0d68a67974dcc707363b2e463ab1fc
johan252642-stack <johan252642@gmail.com>
2026-04-29 06:02:23 +0700

    Update threads.py

--- core/threads.py
+++ core/threads.py
@@ -1,11 +1,15 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def run_threads(func, items, workers=5):
+def run_threads(targets, worker, max_threads=10):
     results = []
 
-    with ThreadPoolExecutor(max_workers=workers) as exe:
-        res = exe.map(func, items)
-        for r in res:
-            results.append(r)
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = [executor.submit(worker, t) for t in targets]
+
+        for f in as_completed(futures):
+            try:
+                results.extend(f.result())
+            except:
+                pass
 
     return results

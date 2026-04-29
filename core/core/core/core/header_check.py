2a2e55a0e0a979c06a42b70f17cead72214c764f
johan252642-stack <johan252642@gmail.com>
2026-04-29 15:01:37 +0700

    Create header_check.py

---
+++ core/core/core/core/header_check.py
@@ -0,0 +1,19 @@
+from core.utils import get
+
+HEADERS = ["X-Frame-Options","Content-Security-Policy"]
+
+def check_headers(url):
+    issues = []
+
+    r = get(url)
+    if not r:
+        return issues
+
+    for h in HEADERS:
+        if h not in r.headers:
+            issues.append({
+                "type":"Header Missing",
+                "detail":h
+            })
+
+    return issues

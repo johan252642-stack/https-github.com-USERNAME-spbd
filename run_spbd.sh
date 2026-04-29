bfa378c983be522423f01bd6d7d986b2856d17f3
Johansah Revi Adnan <johan252642@gmail.com>
2026-04-29 11:34:43 +0000

    update

--- run_spbd.sh
+++ run_spbd.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
-# aktifkan venv
 if [ -f "$DIR/venv/bin/activate" ]; then
     source "$DIR/venv/bin/activate"
 fi
 
-# jalankan tool (FIX PATH)
 python3 "$DIR/spbd.py" "$@"

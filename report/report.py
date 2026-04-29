02d5447334da6e51cfc64f9c0523d2c156b83cd7
johan252642-stack <johan252642@gmail.com>
2026-04-29 15:04:24 +0700

    Update report.py

--- report/report.py
+++ report/report.py
@@ -1,11 +1,5 @@
-def generate_report(data):
-    html = f"""
-    <html><body style='background:#111;color:white;font-family:sans-serif'>
-    <h1>SPBD REPORT</h1>
-    <p>Target: {data['target']}</p>
-    {''.join([f"<p>[{v['severity']}] {v['type']}</p>" for v in data["vulns"]])}
-    </body></html>
-    """
+import json
 
-    open("report.html","w").write(html)
-    print("[✓] Report saved")
+def generate_report(data):
+    with open("report.json","w") as f:
+        json.dump(data,f,indent=4)
